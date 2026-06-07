8f6601efc7b463d0d7eb0975c69da664f8f0953a
DerickPederzini <derickpps@gmail.com>
2026-06-07 19:18:13 -0300

    inicia conexao back e front

--- backend/Horizon/Horizon/src/main/java/com/br/green_horizon/Horizon/analysis/controllers/AnaliseController.java
+++ backend/Horizon/Horizon/src/main/java/com/br/green_horizon/Horizon/analysis/controllers/AnaliseController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("")
+@RestController()
+@RequestMapping("analysis")
 public class AnaliseController {
 
     private final AnaliseFacade facade;
--- backend/Horizon/Horizon/src/main/java/com/br/green_horizon/Horizon/analysis/data/response/CreateAnalysisResponse.java
+++ backend/Horizon/Horizon/src/main/java/com/br/green_horizon/Horizon/analysis/data/response/CreateAnalysisResponse.java
@@ -3,6 +3,8 @@ package com.br.green_horizon.Horizon.analysis.data.response;
 import com.br.green_horizon.Horizon.analysis.application.enums.Clima;
 import com.br.green_horizon.Horizon.analysis.application.enums.Status;
 import com.br.green_horizon.Horizon.analysis.application.enums.TipoPlantio;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +19,11 @@ import java.util.Date;
 public class CreateAnalysisResponse {
     private Long id;
     private Double ndviMedia;
+    @Enumerated(EnumType.STRING)
     private TipoPlantio tipo;
+    @Enumerated(EnumType.STRING)
     private Clima clima;
+    @Enumerated(EnumType.STRING)
     private Status status;
     private String recomendacao;
     private Date date;
