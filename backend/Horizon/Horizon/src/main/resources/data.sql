c08ebe36c5fcfcdcfaa4edbe07bd7c97c5954a07
DerickPederzini <derickpps@gmail.com>
2026-06-08 10:50:46 -0300

    new account

--- backend/Horizon/Horizon/src/main/resources/data.sql
+++ backend/Horizon/Horizon/src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO tb_user(email, password, username) VALUES('conta@gmail.com', '123455', 'Pedro Silva');
+INSERT INTO tb_user(email, password, username) VALUES('a@b', '123', 'Pedro de Souza');
